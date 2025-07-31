0ddc3d80f8c1d9d644a265e9dcbd40a6f494fca4
Juhee <101857307+kimju-hee@users.noreply.github.com>
2025-07-31 09:54:29 +0900

    be/fix : 반품 post URL 수정 (#39)

--- backend/src/main/java/com/yeahyak/backend/controller/ReturnController.java
+++ backend/src/main/java/com/yeahyak/backend/controller/ReturnController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/returns")
+@RequestMapping("/branch/returns")
 public class ReturnController {
 
     private final ReturnService returnService;
