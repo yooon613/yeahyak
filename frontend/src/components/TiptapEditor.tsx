951c4a60074a91bc4c0d5852d39cbbd5fb25fc88
s0nghyunje0ng <oct-2@naver.com>
2025-08-01 11:37:19 +0900

    fe/chore: 잘못 커밋한 파일 복구

--- frontend/src/components/TiptapEditor.tsx
+++ frontend/src/components/TiptapEditor.tsx
@@ -1,23 +1,23 @@
-import { ImageUploadNode } from '@/tiptap/components/tiptap-node/image-upload-node';
-import { Spacer } from '@/tiptap/components/tiptap-ui-primitive/spacer';
-import {
-  Toolbar,
-  ToolbarGroup,
-  ToolbarSeparator,
-} from '@/tiptap/components/tiptap-ui-primitive/toolbar';
-import { HeadingDropdownMenu } from '@/tiptap/components/tiptap-ui/heading-dropdown-menu';
-import { ImageUploadButton } from '@/tiptap/components/tiptap-ui/image-upload-button';
-import { LinkPopover } from '@/tiptap/components/tiptap-ui/link-popover';
-import { ListDropdownMenu } from '@/tiptap/components/tiptap-ui/list-dropdown-menu';
-import { MarkButton } from '@/tiptap/components/tiptap-ui/mark-button';
-import { TextAlignButton } from '@/tiptap/components/tiptap-ui/text-align-button';
-import { UndoRedoButton } from '@/tiptap/components/tiptap-ui/undo-redo-button';
-import { handleImageUpload, MAX_FILE_SIZE } from '@/tiptap/lib/tiptap-utils';
 import { Image } from '@tiptap/extension-image';
 import { TextAlign } from '@tiptap/extension-text-align';
 import { EditorContent, EditorContext, useEditor } from '@tiptap/react';
 import { StarterKit } from '@tiptap/starter-kit';
 import { useEffect } from 'react';
+import { ImageUploadNode } from '../tiptap/components/tiptap-node/image-upload-node';
+import { Spacer } from '../tiptap/components/tiptap-ui-primitive/spacer';
+import {
+  Toolbar,
+  ToolbarGroup,
+  ToolbarSeparator,
+} from '../tiptap/components/tiptap-ui-primitive/toolbar';
+import { HeadingDropdownMenu } from '../tiptap/components/tiptap-ui/heading-dropdown-menu';
+import { ImageUploadButton } from '../tiptap/components/tiptap-ui/image-upload-button';
+import { LinkPopover } from '../tiptap/components/tiptap-ui/link-popover';
+import { ListDropdownMenu } from '../tiptap/components/tiptap-ui/list-dropdown-menu';
+import { MarkButton } from '../tiptap/components/tiptap-ui/mark-button';
+import { TextAlignButton } from '../tiptap/components/tiptap-ui/text-align-button';
+import { UndoRedoButton } from '../tiptap/components/tiptap-ui/undo-redo-button';
+import { handleImageUpload, MAX_FILE_SIZE } from '../tiptap/lib/tiptap-utils';
 import './TipTapEditor.scss';
 
 const TiptapToolbarContent = () => {
